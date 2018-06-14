79e5def2e1395b9232a278ef17ecce39d410489d
Alexander Uskov <neolord.tmb@gmail.com>
2018-06-14 23:52:10 +0200

    preloader

--- js/app.jsx
+++ js/app.jsx
@@ -11,58 +11,56 @@ class App extends React.Component {
     super(props);
     this.state = {
       loading: true,
-      users: [
-        {
-          id: 1
-        }, {
-          id: 2
-        }, {
-          id: 'xxx'
-        }
-      ]
+      users: []
     };
   }
 
   componentDidMount() {
-    API
-      .all('users')
-      .getAll({'since': 0})
-      .then(result => {
-        let users = result
-          .body()
-          .map(v => v.data());
-        console.log(users);
-        this.setState({loading: false, users});
-      })
-      .catch(console.error);
+    setTimeout(() => {
+      API
+        .all('users')
+        .getAll({'since': 0})
+        .then(result => {
+          let users = result
+            .body()
+            .map(v => v.data());
+          console.log(users);
+          this.setState({loading: false, users});
+        })
+        .catch(console.error);
+    }, 2000);
   }
 
   render() {
-    return (
-      <section className="wrapper">
-        <div className="container-fostrap">
-          <div>
-            <h1 className="heading">
-              User list
-            </h1>
-          </div>
-          <div className="content">
-            <div className="container">
-              <div className="row">
-                {this
-                  .state
-                  .users
-                  .map(user => (
-                    <div className="col-xs-12 col-sm-4" key={user.id}>
-                      <UserCard {...user}/>
-                    </div>
-                  ))}
+    return this.state.loading
+      ? (
+        <div id='preloader'></div>
+      )
+      : (
+        <section className="wrapper">
+          <div className="container ">
+            <div className="row">
+              <h1 className="heading">
+                User list
+              </h1>
+            </div>
+            <div className="content row">
+              <div className="container">
+                <div className="row">
+                  {this
+                    .state
+                    .users
+                    .map(user => (
+                      <div className="col-xs-12 col-sm-4" key={user.id}>
+                        <UserCard {...user}/>
+                      </div>
+                    ))}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-    );
+        </section>
+      );
   }
 }
 
